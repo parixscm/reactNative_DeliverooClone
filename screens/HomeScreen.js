e6e9bce511dee4f073164e1e783690b1716220b3
parixscm <parixscm@gmail.com>
2023-02-23 19:21:28 +0900

    🎨 Feat: ADD SearchBox to HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,12 @@
 import { useLayoutEffect } from "react";
-import { Image, SafeAreaView, Text, View } from "react-native";
+import { Image, SafeAreaView, Text, TextInput, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { UserIcon, ChevronDownIcon } from "react-native-heroicons/outline";
+import {
+  UserIcon,
+  ChevronDownIcon,
+  AdjustmentsVerticalIcon,
+  MagnifyingGlassIcon,
+} from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -23,13 +28,25 @@ const HomeScreen = () => {
           className="w-7 h-7 p-4 bg-gray-300 rounded-full"
         />
         <View className="flex-1">
-          <Text className="text-xs text-red-400 font-bold">Deliver Now</Text>
+          <Text className="text-xs text-gray-400 font-bold">Deliver Now</Text>
           <Text className="text-xl font-bold">
             Current Location
             <ChevronDownIcon size={20} color="#00CCBB" />
           </Text>
         </View>
-        <UserIcon size={30} color="#00CCBB" />
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
+
+      {/* Search */}
+      <View className="px-4 pb-2 flex-row items-center space-x-2">
+        <View className="p-3 flex-row flex-1 space-x-2 bg-gray-200">
+          <MagnifyingGlassIcon size={20} kcolor="gray" />
+          <TextInput
+            placeholder="Restaurants and cuisines"
+            keyboardType="default"
+          />
+        </View>
+        <AdjustmentsVerticalIcon color="#00CCBB" />
       </View>
     </SafeAreaView>
   );
