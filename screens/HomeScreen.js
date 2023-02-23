e13d061aca2029769dac868dc10880682832d207
parixscm <parixscm@gmail.com>
2023-02-23 18:41:54 +0900

    🎨 Feat: ADD Header to HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,29 @@
-import { Text, View } from "react-native";
+import { Image, SafeAreaView, Text, View } from "react-native";
+import { UserIcon, ChevronDownIcon } from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView className="bg-white">
+      {/* Header */}
+      <View className="flex-row pb-3 mx-4 items-center space-x-2">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="w-7 h-7 p-4 bg-gray-300 rounded-full"
+        />
+
+        <View className="flex-1">
+          <Text className="text-xs text-red-400 font-bold">Deliver Now</Text>
+          <Text className="text-xl font-bold">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+
+        <UserIcon size={30} color="#00CCBB" />
+      </View>
+    </SafeAreaView>
   );
 };
 
