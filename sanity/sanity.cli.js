5cc7840230921847fff4b217c9451ebaa5545695
parixscm <parixscm@gmail.com>
2023-02-25 04:57:39 +0900

    🔥 Remove: DELETE sanity cms related files

--- sanity/sanity.cli.js
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'yet6izo4',
-    dataset: 'production'
-  }
-})
--- sanity/schemas/index.js
+++
@@ -1,6 +0,0 @@
-import restaurant from './restaurant'
-import category from './category'
-import featured from './featured'
-import dish from './dish'
-
-export const schemaTypes = [restaurant, category, featured, dish]
