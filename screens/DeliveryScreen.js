f31aad4922df7d3539a58641e16c233c3dde9a78
parixscm <parixscm@gmail.com>
2023-03-02 16:13:08 +0900

    ✨ Fix: ADD map and pin functionality

--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -9,6 +9,8 @@ import {
 import { useSelector } from "react-redux";
 import { selectRestaurant } from "../features/restaurantSlice";
 import { XMarkIcon } from "react-native-heroicons/solid";
+import MapView, { Marker } from "react-native-maps";
+``;
 import * as Progress from "react-native-progress";
 
 const DeliveryScreen = () => {
@@ -35,13 +37,43 @@ const DeliveryScreen = () => {
               className="w-20 h-20"
             />
           </View>
-
           <Progress.Bar size={30} color="#00CCBB" indeterminate />
-
           <Text className="mt-3 text-gray-500">
             Your order at {restaurant.title} is being prepared
           </Text>
         </View>
+      </SafeAreaView>
+      <MapView
+        initialRegion={{
+          latitude: restaurant.lat,
+          longitude: restaurant.long,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        }}
+        className="mt-10 flex-1 z-0"
+        mapType="mutedStandard"
+      >
+        <Marker
+          coordinate={{
+            latitude: restaurant.lat,
+            longitude: restaurant.long,
+          }}
+          title={restaurant.title}
+          description={restaurant.short_description}
+          identifier="origin"
+          pinColor="#00CCBB"
+        />
+      </MapView>
+      <SafeAreaView className="h-28 flex-row items-center space-x-5 bg-white">
+        <Image
+          source={{ uri: "https://links.papareact.com/wru" }}
+          className="p-4 ml-5 w-12 h-12 bg-gray-300 rounded-full"
+        />
+        <View className="flex-1">
+          <Text className="text-lg">Jason Kim</Text>
+          <Text className="text-gray-400">Your Rider</Text>
+        </View>
+        <Text className="mr-5 text-[#00CCBB] text-lg font-bold">Call</Text>
       </SafeAreaView>
     </View>
   );
